0bf33050b73d703933fc0f6ca947d18a0b68e927
Vit Safarik <vit@safarik.dev>
2020-11-07 17:46:45 +0100

    save data to local storage

--- MOVIE-SEAT-BOOKING/script.js
+++ MOVIE-SEAT-BOOKING/script.js
@@ -9,6 +9,10 @@ let ticketPrice = +movieSelect.value;
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
